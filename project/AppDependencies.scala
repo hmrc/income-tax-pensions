3db590689c808c55e620268d20cbadca710b2305
salman-a-dev <salman.a.dev@gmail.com>
2022-08-24 10:53:10 +0100

    [SASS-3236] Upgrading major dependencies.
    Updated various dependencies to match the current catalogue:
    
    https://catalogue.tax.service.gov.uk/service/income-tax-pensions
    
    This required updating all integration tests to now take in a bearer token as part of header.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,19 @@ import sbt._
 
 object AppDependencies {
 
+  private val bootstrapPlay28Versions = "7.1.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % "5.19.0",
+    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % bootstrapPlay28Versions,
     "com.fasterxml.jackson.module"  %%  "jackson-module-scala"      % "2.12.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.19.0"             % Test,
-    "com.typesafe.play"       %% "play-test"                % current              % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.9"              % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"             % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"              % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0"                % "test, it",
-    "org.scalamock"           %% "scalamock"                % "5.1.0"              % Test)
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapPlay28Versions             % Test,
+    "com.typesafe.play"       %% "play-test"                % current                             % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.9"                             % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"                            % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                             % "test, it",
+    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0"                            % "test, it",
+    "org.scalamock"           %% "scalamock"                % "5.1.0"                             % Test)
 }
