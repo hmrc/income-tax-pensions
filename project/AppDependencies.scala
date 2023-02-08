b29ff0a84cc92685f78794e94193aeabe4f2cdbf
edem-nsefik-hmrc <edem.nsefik2@digital.hmrc.gov.uk>
2023-02-08 11:35:39 +0000

    SASS-4234: Update Platform dependencies
    Update in line with Tax Platform catalogue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,19 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlay28Versions = "7.1.0"
+  private val bootstrapPlay28Versions = "7.13.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % bootstrapPlay28Versions,
-    "com.fasterxml.jackson.module"  %%  "jackson-module-scala"      % "2.12.2"
+    "com.fasterxml.jackson.module"  %%  "jackson-module-scala"      % "2.14.2"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapPlay28Versions             % Test,
     "com.typesafe.play"       %% "play-test"                % current                             % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.9"                             % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"                            % "test, it",
+    "org.scalatest"           %% "scalatest"                % "3.2.15"                             % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.0"                            % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                             % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0"                            % "test, it",
-    "org.scalamock"           %% "scalamock"                % "5.1.0"                             % Test)
+    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.35.0"                            % "test, it",
+    "org.scalamock"           %% "scalamock"                % "5.2.0"                             % Test)
 }
