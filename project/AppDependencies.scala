138b27819b2fe669a346d60524858873ca49032b
muhammad-ali-ahmad <17907161+Muhammad-Ali-Ahmad@users.noreply.github.com>
2026-02-27 23:16:24 +0000

    dependencies updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
   private val flexmarkVersion  = "0.64.8"
   private val hmrcMongoVersion = "2.12.0"
 
@@ -33,10 +33,10 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.21.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.21.1",
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
-    "com.beachape"                 %% "enumeratum"                % "1.9.4",
-    "com.beachape"                 %% "enumeratum-play-json"      % "1.9.4" excludeAll (jacksonAndPlayExclusions *)
+    "com.beachape"                 %% "enumeratum"                % "1.9.5",
+    "com.beachape"                 %% "enumeratum-play-json"      % "1.9.5" excludeAll (jacksonAndPlayExclusions *)
   )
 
   val test: Seq[ModuleID] = Seq(
